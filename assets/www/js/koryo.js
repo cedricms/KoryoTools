86601efc6b33e5dc2b967bb3cf4ef82af94e1f4b
cedricms <cedric@maria-sube.com>
2014-04-27 14:30:02 +0200

    Init language in settings on start of the app.

--- assets/www/js/koryo.js
+++ assets/www/js/koryo.js
@@ -47,11 +47,46 @@ function initTabLinkActions() {
 }
 
 function initLanguages() {
-	var userLang = navigator.language || navigator.userLanguage;
-	userLang = userLang.substring(0,2);
-	loadBundles(userLang);
+  var userLang = navigator.language || navigator.userLanguage;
+  userLang = userLang.substring(0,2);
+  loadBundles(userLang);
+  
+  var settingForm = document.getElementById("settingForm");
+  languageSelect = settingForm.elements["language"];
+  setSelectedOptionStringValue(languageSelect, userLang);
 }
 
-function saveLanguage() {
+function getSelectedOptionIntValue(selectComponent) {
+	var result = 0;
 	
+	var optionValue = selectComponent.options[selectComponent.selectedIndex].value;
+	
+	if (!isNaN(optionValue)) {
+        result = parseInt(optionValue);
+    } // if
+  
+	return result;
+}
+
+function getSelectedOptionStringValue(selectComponent) {
+	var result = '';
+	
+	var optionValue = selectComponent.options[selectComponent.selectedIndex].value;
+	
+	return result;
+}
+
+function setSelectedOptionStringValue(selectComponent, value) {
+	var len = selectComponent.options.length;
+    
+    for (var i=0; i<len; i++){
+      var lang = selectComponent.options[i].value;
+	  if (selectComponent.options[i].value === value) {
+		  selectComponent.options[i].selected = true;
+	  } // if
+    } // for
+}
+
+function saveSettings() {
+  alert('Save settings');
 }
